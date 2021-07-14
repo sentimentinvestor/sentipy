460829c0d2a48d47f93064fc45537fb61367895d
Timothy Langer <zeevox.dev@gmail.com>
2021-07-14 14:13:50 +0100

    Update SentiPy to use the v4 API
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

--- sentipy/sentipy.py
+++ sentipy/sentipy.py
@@ -1,90 +1,191 @@
+from json import JSONDecodeError
+
 import requests
 
-class Sentipy():
-  base_url = r"https://sentimentinvestor.com/api/v3/"
 
-  def __init__(self, token=None, key=None):
-    if token is None or key is None:
-      raise ValueError("Please provide a token and key - these can be obtained at https://sentimentinvestor.com/developer/home")
-    self.token = token
-    self.key = key
+class Sentipy:
+    base_url = r"https://api.sentimentinvestor.com/v4/"
 
-  def base_request(self, endpoint, params={}):
-    url = self.base_url + endpoint
-    params["token"] = self.token
-    params["key"] = self.key
-    response = requests.get(url, params)
-    if response.content.decode("utf-8")  == 'invalid_parameter' or response.content.decode("utf-8")  == 'incorrect_key':
-      raise ValueError("Incorrect key or token")
-    elif response.ok:
-      return response.json()
-    else:
-      raise Exception("No response from the server - check your Wifi or try again in 15 minutes")
+    def __init__(self, token=None, key=None):
+        """
+        Initialise a new SentiPy instance with the provided token and key
+        :param token: token is a string obtained from SentimentInvestor website
+        :param key: key is a string obtained from SentimentInvestor website
+        """
+        if token is None or key is None:
+            raise ValueError(
+                "Please provide a token and key - these can be obtained at "
+                "https://sentimentinvestor.com/developer/dashboard")
+        self.token = token
+        self.key = key
 
-  def quote(self, symbol):
-    params = {
-      "symbol": symbol
-    }
-    response = self.base_request("quote", params=params)
-    return QuoteData(response)
+    def __base_request(self, endpoint, params=None):
+        """
+        Make a request to a specific REST endpoint on the SentimentInvestor API
+        :param endpoint: The final __fragment specifying the endpoint to call
+        :param params: A dictionary of any necessary parameters
+        :return: returns the JSON response if the request was successful, otherwise None
+        """
+        if params is None:
+            params = {}
+        url = self.base_url + endpoint
+        params["token"] = self.token
+        params["key"] = self.key
+        response = requests.get(url, params)
+        if response.content.decode("utf-8") == 'invalid_parameter' \
+                or response.content.decode("utf-8") == 'incorrect_key':
+            raise ValueError("Incorrect key or token")
+        else:
+            try:
+                json = response.json()
+            except JSONDecodeError:
+                raise Exception(response.text)
 
-  def sort(self, metric, limit):
-    params = {
-      "metric": metric,
-      "limit": limit
-    }
-    response = self.base_request("sort", params=params)
-    return SortData(response)
+            if response.ok:
+                return json
+            else:
+                raise Exception(json.get("message"))
 
-  def historical(self, symbol, metric):
-    params = {
-      "symbol": symbol,
-      "metric": metric
-    }
-    response = self.base_request("historical", params=params)
-    return HistoricalData(response)
+    def parse(self, symbol):
+        """
+        The parsed data endpoints provides the four core metrics for a stock: AHI, RHI, SGP and sentiment.
+        :param symbol: String specifying the ticker or symbol of the stock to request data for
+        :return: returns a QuoteData object
+        """
+        params = {
+            "symbol": symbol
+        }
+        response = self.__base_request("parsed", params=params)
+        return QuoteData(response)
 
-class BasicTicker():
-  def __init__(self, json):
-    self.ticker = json.get("ticker")
-    self.AHI = json.get("AHI")
-    self.sentiment = json.get("sentiment")
-    self.RHI = json.get("RHI")
-    self.SGP = json.get("SGP")
+    def raw(self, symbol):
+        """
+        The raw data endpoint provides access to raw data metrics for the monitored social platforms
+        :param symbol: String specifying the ticker or symbol of the stock to request data for
+        :return: returns a QuoteData object
+        """
+        params = {
+            "symbol": symbol
+        }
+        response = self.__base_request("raw", params=params)
+        return QuoteData(response)
 
-class QuoteData(BasicTicker):
-  def __init__(self, json):
-    super().__init__(json)
-    self.last_update = json.get("last_update")
-    self.reddit_comment_mentions = json.get("reddit_comment_mentions")
-    self.reddit_comment_sentiment = json.get("reddit_comment_sentiment")
-    self.reddit_post_mentions = json.get("reddit_post_mentions")
-    self.reddit_post_sentiment = json.get("reddit_post_sentiment")
-    self.tweet_mentions = json.get("tweet_mentions")
-    self.tweet_sentiment = json.get("tweet_sentiment")
-    self.yahoo_finance_mentions = json.get("yahoo_finance_mentions")
-    self.yahoo_finance_sentiment = json.get("yahoo_finance_mentions")
-    self.stocktwits_mentions = json.get("stocktwits_mentions")
-    self.stocktwits_sentiment = json.get("stocktwits_sentiment")
+    def quote(self, symbol, enrich=False):
+        """
+        The quote data endpoint provides access to all realtime data about stocks along with further data if requested
+        :param symbol: String specifying the ticker or symbol of the stock to request data for
+        :param enrich: Set this boolean to true to request enriched data
+        :return: returns a QuoteData object
+        """
+        params = {
+            "symbol": symbol,
+            "enrich": enrich
+        }
+        response = self.__base_request("quote", params=params)
+        return QuoteData(response)
 
-class HistoricalData():
-  def __init__(self, json):
-    self.history = [HistoricalDatapoint(dp) for dp in json]
-    self.sort()
+    def sort(self, metric, limit):
+        """
+        The sort data endpoint provides access to ordered rankings of stocks across core metrics
+        :param metric: The metric by which to sort the stocks.
+        This can be any of the metrics returned by a quote request.
+        :param limit: The maximum number of stocks to return
+        :return: returns a list of TickerData objects
+        """
+        params = {
+            "metric": metric,
+            "limit": limit
+        }
+        response = self.__base_request("sort", params=params)
+        return [TickerData(dp) for dp in response.get("results")]
 
-  def sort(self, direction="asc"):
-    if direction=="asc":
-      self.history.sort(key=lambda x: x.timestamp)
-    else:
-      self.history.sort(reverse=True, key=lambda x: x.timestamp)
+    def historical(self, symbol, metric, start, end):
+        """
+        The historical data endpoint provides access to historical data for stocks
+        :param symbol: The stock to look up historical data for
+        :param metric: The metric for which to return data.
+        This can be any of the metrics returned by a quote request.
+        :param start: string or int Unix epoch timestamp in seconds specifying start of date range
+        :param end: string or int Unix epoch timestamp in seconds specifying end of date range
+        :return: a dict with timestamp -> data entry mappings
+        """
+        params = {
+            "symbol": symbol,
+            "metric": metric,
+            "start": start,
+            "end": end
+        }
+        response = self.__base_request("historical", params=params)
+        return {dp.get("timestamp"): dp.get("data") for dp in response.get("results")}
 
-class HistoricalDatapoint():
-  def __init__(self, datapoint):
-    self.data = datapoint.get("data")
-    self.timestamp = datapoint.get("timestamp")
+    def bulk(self, symbols, enrich=False):
+        """
+        Get quote data for several stocks simultaneously
+        :param symbols: iterable of stocks to get quote data for
+        :param enrich: boolean whether to get enriched data
+        :return: a list of TickerData objects
+        """
+        params = {
+            "symbols": ",".join(symbols),
+            "enrich": enrich
+        }
+        response = self.__base_request("bulk", params=params)
+        return [TickerData(result) for result in response.get("results")]
+    
+    def all(self, enrich=False):
+        """
+        Get all data for all stocks simultaneously. 
+        Note that this call runs on the main thread and can take up to a minute to execute.
+        :param enrich: boolean whether to fetch enriched data
+        :return: a list of TickerData objects
+        """
+        params = {
+            "enrich": enrich
+        }
+        response = self.__base_request("all", params=params)
+        return [TickerData(result) for result in response.get("results")]
 
-class SortData():
-  def __init__(self, json):
-    self.sort = [BasicTicker(dp) for dp in json]
+    def supported(self, symbol):
+        """
+        Query whether SentimentInvestor has data for a specified stock
+        :param symbol: The stock ticker symbol to query
+        :return: boolean whether supported or not
+        """
+        return self.__base_request("supported", params={"symbol": symbol}).get("result")
+
+    def all_stocks(self):
+        """
+        Get a list of all stocks for which Sentiment gather data
+        :return: iterable list of stock symbols
+        """
+        return self.__base_request("all-stocks").get("results")
 
 
+class TickerData:
+    """
+    Specifies a basic data item for a specific ticker
+    Do not try to initialise one yourself.
+    """
+
+    def __init__(self, json):
+        # for every metric returned in the json set it as an attribute for this object
+        for k, v in json.items():
+            # do not create a results parameter if present as this is handled by derived classes separately
+            if k == "results":
+                continue
+            setattr(self, k, v)
+
+    def __str__(self):
+        return str({k: self.__getattribute__(k) for k in dir(self) if not k.startswith("__")})
+
+
+class QuoteData(TickerData):
+    """
+    For a list of available metrics, use dir(object)
+    Returned by quote, do not try to initialise one yourself.
+    """
+
+    def __init__(self, json):
+        super().__init__(json)
+        for k, v in json.get("results").items():
+            setattr(self, k, v)
