f37d74dbc4bae711b062397a8ac485c6921a1cb8
Timothy Langer <zeevox.dev@gmail.com>
2021-08-11 11:05:02 +0100

    Add more extensive .gitignore
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

--- tests/sentipy_tests.py
+++ tests/sentipy_tests.py
@@ -80,7 +80,7 @@ class SentipyTestCase(unittest.TestCase):
     @vcr.use_cassette("vcr_cassettes/parsed.yml")  # type: ignore[misc]
     @beartype
     def test_parsed(self) -> None:
-        """Tests SentiPy's parsed attribute."""
+        """Tests SentiPy's `parsed` method."""
         data = self.sentipy.parsed("AAPL")
         self.check_basics(data)
         self.assertHasAttrs(data, ["sentiment", "AHI", "RHI", "SGP"])
@@ -88,7 +88,7 @@ class SentipyTestCase(unittest.TestCase):
     @vcr.use_cassette("vcr_cassettes/raw.yml")  # type: ignore[misc]
     @beartype
     def test_raw(self) -> None:
-        """Tests SentiPy's raw attribute."""
+        """Tests SentiPy's `raw` method."""
         data = self.sentipy.raw("AAPL")
         self.check_basics(data)
         self.assertHasAttrs(
@@ -109,7 +109,7 @@ class SentipyTestCase(unittest.TestCase):
 
     @vcr.use_cassette("vcr_cassettes/quote.yml")  # type: ignore[misc]
     def test_quote(self) -> None:
-        """Tests SentiPy's quote attribute."""
+        """Tests SentiPy's `quote` method."""
         data = self.sentipy.quote("AAPL")
         self.check_basics(data)
         self.assertHasAttrs(
@@ -135,7 +135,7 @@ class SentipyTestCase(unittest.TestCase):
     @vcr.use_cassette("vcr_cassettes/bulk.yml")  # type: ignore[misc]
     @beartype
     def test_bulk(self) -> None:
-        """Tests SentiPy's bulk attribute."""
+        """Tests SentiPy's `bulk` method."""
         data = self.sentipy.bulk(["AAPL", "TSLA", "PYPL"])
         self.assertEqual(len(data), 3)
         for stock in data:
